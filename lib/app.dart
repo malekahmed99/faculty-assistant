302443df48515bdea89f041b1353831ac9f50062
Mohamed Ehab <mehab1638@gmail.com>
2026-04-16 21:58:25 +0200

    solve some codes in main dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,27 +2,10 @@ import 'package:ai_campus_guide/core/theme/app_colors.dart';
 import 'package:ai_campus_guide/features/home_feature/presentation/view/home_screen.dart';
 import 'package:ai_campus_guide/features/chat_bot_feature/presentation/view/chat_bot_screen.dart';
 import 'package:ai_campus_guide/features/service_feature/presentation/view/service_screen.dart';
-import 'package:ai_campus_guide/features/splash_feature/presentation/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/theme/app_theme.dart';
-import 'core/utils/constants.dart';
 import 'features/map_feature/presentation/view/map_screen.dart';
 import 'core/utils/bottom_nav.dart';
-
-class AICampusGuideApp extends StatelessWidget {
-  const AICampusGuideApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppConstants.appName,
-      theme: AppTheme.lightTheme,
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
-    );
-  }
-}
 
 class MainNavigationScreen extends ConsumerStatefulWidget {
   const MainNavigationScreen({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
+import 'package:ai_campus_guide/core/theme/app_theme.dart';
+import 'package:ai_campus_guide/core/utils/constants.dart';
+import 'package:ai_campus_guide/features/splash_feature/presentation/view/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    const ProviderScope(
-      child: AICampusGuideApp(),
-    ),
+    const AICampusGuideApp(),
   );
+}
+
+class AICampusGuideApp extends StatelessWidget {
+  const AICampusGuideApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppConstants.appName,
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
 }
